cdf29318641d48aa775254ae207a2e606f927f22
akashbhardwaj04 <akkiworkspace@gmail.com>
2020-09-06 22:41:14 +0530

    updated contract mytoken

--- contracts/mytoken.sol
+++ contracts/mytoken.sol
@@ -13,28 +13,3 @@ contract DappToken {
         uint256 _value
     );
 
-    event Approval(
-        address indexed _owner,
-        address indexed _spender,
-        uint256 _value
-    );
-
-    mapping(address => uint256) public balanceOf;
-    mapping(address => mapping(address => uint256)) public allowance;
-
-    function DappTokens (uint256 _initialSupply) public {
-        balanceOf[msg.sender] = _initialSupply;
-        totalSupply = _initialSupply;
-    }
-
-    function transfer(address _to, uint256 _value) public returns (bool success) {
-        require(balanceOf[msg.sender] >= _value);
-
-        balanceOf[msg.sender] -= _value;
-        balanceOf[_to] += _value;
-
-        emit Transfer(msg.sender, _to, _value);
-
-        return true;
-    }
-
