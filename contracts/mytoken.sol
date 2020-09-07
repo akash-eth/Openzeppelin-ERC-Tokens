fc05a061631ceeb853c71ede49603a09b9503db4
Akash Bhardwaj <48449366+akashbhardwaj04@users.noreply.github.com>
2020-09-07 23:29:10 +0530

    Update mytoken.sol

--- contracts/mytoken.sol
+++ contracts/mytoken.sol
@@ -7,9 +7,5 @@ contract DappToken {
     string  public standard = "DApp Token v1.0";
     uint256 public totalSupply;
 
-    event Transfer(
-        address indexed _from,
-        address indexed _to,
-        uint256 _value
-    );
 
+}
